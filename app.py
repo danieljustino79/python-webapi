fe977049955c24724fee32c993820cda6ad086a0
Daniel Justino <daniel.justino@guiainvest.com.br>
2020-08-03 16:02:47 -0300

    Remove vars global.

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
-from application import user
 from bottle import get, post, request, route, run
-#import json
+from application.user import User
 
 
 @post('/user')
 def add():
-    '''
-    name = request.forms.get('name')
-    email = request.forms.get('email')
-    '''
-
     body = request.json
     name = body.get('name')
     email = body.get('email')
 
-    if user.add(name, email):
-        return 'ok'
+    _user = User()
+    _id = _user.add(name, email)
+    if isinstance(_id, int):
+        return str(_id)
     else:
         return 'error'
 
 
-@post('/user/<id:int>/permission')
-def add_permission(id):
+@post('/user/<id_:int>/permission')
+def add_permission(id_):
     body = request.json
     permissions = body.get('permissions')
 
-    if user.add_permission(id,  permissions):
-        return 'ok'
+    _user = User()
+    id_validate = _user.add_permission(id_, permissions)
+    if isinstance(id_validate, int):
+        return str(id_validate)
     else:
         return 'error'
 
 
 @get('/user')
 def get():
-    return user.get()
+    _user = User()
+    return _user.get_users()
 
 
-@route('/user/<id:int>')
-def getbyid(id):
-    return user.get_by_id(id)
-
-
+@route('/user/<id_:int>')
+def get_by_id(id_):
+    _user = User()
+    _obj = _user.get_by_id(id_)
+    if isinstance(_obj, object):
+        return _obj
+    else:
+        return 'error'
 
 
 run(host='localhost', port=8081, debug=True)
--- application/user.py
+++ application/user.py
@@ -1,58 +1,43 @@
-users = {}
+class User:
+    users = {}
 
+    def _contain_id(self, id_):
+        return id_ in self.users
 
-def next_id():
-    global users
-    lenght = len(users)
-    if lenght == 0:
-        return str(1)
-    else:
-        return str(lenght + 1)
+    def _next_id(self):
+        self.users = self.get_users()
+        _lenght = len(self.users)
+        if _lenght == 0:
+            return 1
+        else:
+            return _lenght + 1
 
+    def add(self, name, email):
+        if (len(name) < 2) or (len(email) < 3):
+            return None
+        else:
+            next_id = self._next_id()
+            _user = {
+                'id': next_id,
+                'name': name,
+                'email': email
+            }
+            self.users[next_id] = _user
+            return next_id
 
-def contain_id(id):
-    return str(id) in users
+    def add_permission(self, id_, permissions):
+        _list = list(permissions)
+        if isinstance(id_, int) and self._contain_id(id_) and isinstance(_list, list):
+            self.users[id_]['permissions'] = _list
+            return id_
+        else:
+            return None
 
+    def get_by_id(self, id_):
+        if isinstance(id_, int) and self._contain_id(id_):
+            return self.users[id_]
+        else:
+            return {}
 
-def add(name, email):
-    global users
-    nextId = next_id()
-    users[nextId] = {
-        'id': nextId,
-        'name': name,
-        'email': email
-    }
-
-    return True
-
-
-def add_permission(id, permissions):
-    global users
-    if isinstance(id, int) and contain_id(id) and isinstance(permissions, list):
-        users[str(id)]['permissions'] = list(permissions)
-        return True
-    else:
-        return False
-
-
-def get():
-    return users
-
-
-def get_by_id(id):
-    global users
-    if isinstance(id, int) and contain_id(id):
-        index = str(id)
-        return users[index]
-    else:
-        return {}
-
-
-def remove(id):
-    global users
-    if str(id) in users:
-        users.pop(id)
-        return True
-
-    return False
-
+    def get_users(self):
+        return self.users
